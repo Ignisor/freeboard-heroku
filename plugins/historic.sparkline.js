dcbb131d97fcf58c487aaca0f73c6b55c8a87a23
Ignisor <ignis2497@gmail.com>
2019-02-20 12:13:37 +0200

    historic sparkline

---
+++ plugins/historic.sparkline.js
@@ -0,0 +1,272 @@
+
+(function () {
+	var SPARKLINE_HISTORY_LENGTH = 100;
+	var SPARKLINE_COLORS = ["#FF9900", "#FFFFFF", "#B3B4B4", "#6B6B6B", "#28DE28", "#13F7F9", "#E6EE18", "#C41204", "#CA3CB8", "#0B1CFB"];
+
+    function easeTransitionText(newValue, textElement, duration) {
+
+		var currentValue = $(textElement).text();
+
+        if (currentValue == newValue)
+            return;
+
+        if ($.isNumeric(newValue) && $.isNumeric(currentValue)) {
+            var numParts = newValue.toString().split('.');
+            var endingPrecision = 0;
+
+            if (numParts.length > 1) {
+                endingPrecision = numParts[1].length;
+            }
+
+            numParts = currentValue.toString().split('.');
+            var startingPrecision = 0;
+
+            if (numParts.length > 1) {
+                startingPrecision = numParts[1].length;
+            }
+
+            jQuery({transitionValue: Number(currentValue), precisionValue: startingPrecision}).animate({transitionValue: Number(newValue), precisionValue: endingPrecision}, {
+                duration: duration,
+                step: function () {
+                    $(textElement).text(this.transitionValue.toFixed(this.precisionValue));
+                },
+                done: function () {
+                    $(textElement).text(newValue);
+                }
+            });
+        }
+        else {
+            $(textElement).text(newValue);
+        }
+    }
+
+	function addSparklineLegend(element, legend) {
+		var legendElt = $("<div class='sparkline-legend'></div>");
+		for(var i=0; i<legend.length; i++) {
+			var color = SPARKLINE_COLORS[i % SPARKLINE_COLORS.length];
+			var label = legend[i];
+			legendElt.append("<div class='sparkline-legend-value'><span style='color:" +
+							 color + "'>&#9679;</span>" + label + "</div>");
+		}
+		element.empty().append(legendElt);
+
+		freeboard.addStyle('.sparkline-legend', "margin:5px;");
+		freeboard.addStyle('.sparkline-legend-value',
+			'color:white; font:10px arial,san serif; float:left; overflow:hidden; width:50%;');
+		freeboard.addStyle('.sparkline-legend-value span',
+			'font-weight:bold; padding-right:5px;');
+	}
+
+	function addValueToSparkline(element, value, legend) {
+		var values = $(element).data().values;
+		var valueMin = $(element).data().valueMin;
+		var valueMax = $(element).data().valueMax;
+		if (!values) {
+			values = [];
+			valueMin = undefined;
+			valueMax = undefined;
+		}
+
+		var collateValues = function(val, plotIndex) {
+			if(!values[plotIndex]) {
+				values[plotIndex] = [];
+			}
+			if (values[plotIndex].length >= SPARKLINE_HISTORY_LENGTH) {
+				values[plotIndex].shift();
+			}
+			values[plotIndex].push(Number(val));
+
+			if(valueMin === undefined || val < valueMin) {
+				valueMin = val;
+			}
+			if(valueMax === undefined || val > valueMax) {
+				valueMax = val;
+			}
+		}
+
+		if(_.isArray(value)) {
+			_.each(value, collateValues);
+		} else {
+			collateValues(value, 0);
+		}
+		$(element).data().values = values;
+		$(element).data().valueMin = valueMin;
+		$(element).data().valueMax = valueMax;
+
+		var tooltipHTML = '<span style="color: {{color}}">&#9679;</span> {{y}}';
+
+		var composite = false;
+		_.each(values, function(valueArray, valueIndex) {
+			$(element).sparkline(valueArray, {
+				type: "line",
+				composite: composite,
+				height: "100%",
+				width: "100%",
+				fillColor: false,
+				lineColor: SPARKLINE_COLORS[valueIndex % SPARKLINE_COLORS.length],
+				lineWidth: 2,
+				spotRadius: 3,
+				spotColor: false,
+				minSpotColor: "#78AB49",
+				maxSpotColor: "#78AB49",
+				highlightSpotColor: "#9D3926",
+				highlightLineColor: "#9D3926",
+				chartRangeMin: valueMin,
+				chartRangeMax: valueMax,
+				tooltipFormat: (legend && legend[valueIndex])?tooltipHTML + ' (' + legend[valueIndex] + ')':tooltipHTML
+			});
+			composite = true;
+		});
+	}
+
+	var valueStyle = freeboard.getStyleString("values");
+
+	freeboard.addStyle('.widget-big-text', valueStyle + "font-size:75px;");
+
+	freeboard.addStyle('.tw-display', 'width: 100%; height:100%; display:table; table-layout:fixed;');
+
+	freeboard.addStyle('.tw-tr',
+		'display:table-row;');
+
+	freeboard.addStyle('.tw-tg',
+		'display:table-row-group;');
+
+	freeboard.addStyle('.tw-tc',
+		'display:table-caption;');
+
+	freeboard.addStyle('.tw-td',
+		'display:table-cell;');
+
+	freeboard.addStyle('.tw-value',
+		valueStyle +
+		'overflow: hidden;' +
+		'display: inline-block;' +
+		'text-overflow: ellipsis;');
+
+	freeboard.addStyle('.tw-unit',
+		'display: inline-block;' +
+		'padding-left: 10px;' +
+		'padding-bottom: 1.1em;' +
+		'vertical-align: bottom;');
+
+	freeboard.addStyle('.tw-value-wrapper',
+		'position: relative;' +
+		'vertical-align: middle;' +
+		'height:100%;');
+
+	freeboard.addStyle('.tw-sparkline',
+		'height:20px;');
+
+	freeboard.addStyle('.sparkline', "width:100%;height: 75px;");
+    var sparklineWidget = function (settings) {
+        var self = this;
+
+        var titleElement = $('<h2 class="section-title"></h2>');
+        var sparklineElement = $('<div class="sparkline"></div>');
+		var sparklineLegend = $('<div></div>');
+		var currentSettings = settings;
+
+        this.render = function (element) {
+            $(element).append(titleElement).append(sparklineElement).append(sparklineLegend);
+        }
+
+        this.onSettingsChanged = function (newSettings) {
+			currentSettings = newSettings;
+            titleElement.html((_.isUndefined(newSettings.title) ? "" : newSettings.title));
+
+			if(newSettings.include_legend) {
+				addSparklineLegend(sparklineLegend,  newSettings.legend.split(","));
+			}
+        }
+
+        this.onCalculatedValueChanged = function (settingName, newValue) {
+            if (Array.isArray(newValue[0]) && newValue[0].length) {
+                // clear current values to refresh
+                delete $(sparklineElement).data().values;
+
+                // get number of points to plot for the series
+                // assume all series have the same number of points ??
+                length = newValue[0].length;
+                for (i = 0; i < length; i++) {
+                    // empty the series points
+                    arrayvalues = [];
+
+                    // add a point from each series
+                    for (j = 0; j < newValue.length; j++)
+                        arrayvalues.push(newValue[j][i].value);
+
+                    // now pass the serries of points into the plot routine
+                    if (currentSettings.legend) {
+                        addValueToSparkline(sparklineElement, arrayvalues, currentSettings.legend.split(","));
+                    } else {
+                        addValueToSparkline(sparklineElement, arrayvalues);
+                    }
+                }
+            }
+            else {
+                if (currentSettings.legend) {
+                    addValueToSparkline(sparklineElement, newValue, currentSettings.legend.split(","));
+                } else {
+
+                    addValueToSparkline(sparklineElement, newValue);
+
+                }
+
+            }
+        }
+
+        this.onDispose = function () {
+        }
+
+        this.getHeight = function () {
+			var legendHeight = 0;
+			if (currentSettings.include_legend && currentSettings.legend) {
+				var legendLength = currentSettings.legend.split(",").length;
+				if (legendLength > 4) {
+					legendHeight = Math.floor((legendLength-1) / 4) * 0.5;
+				} else if (legendLength) {
+					legendHeight = 0.5;
+				}
+			}
+			return 2 + legendHeight;
+        }
+
+        this.onSettingsChanged(settings);
+    };
+
+    freeboard.loadWidgetPlugin({
+        type_name: "historical_sparkline",
+        display_name: "Historical Sparkline",
+        "external_scripts" : [
+            "plugins/thirdparty/jquery.sparkline.min.js"
+        ],
+        settings: [
+            {
+                name: "title",
+                display_name: "Title",
+                type: "text"
+            },
+            {
+                name: "value",
+                display_name: "Value",
+                type: "calculated",
+				multi_input: "true"
+            },
+			{
+				name: "include_legend",
+				display_name: "Include Legend",
+				type: "boolean"
+			},
+			{
+				name: "legend",
+				display_name: "Legend",
+				type: "text",
+				description: "Comma-separated for multiple sparklines"
+			}
+        ],
+        newInstance: function (settings, newInstanceCallback) {
+            newInstanceCallback(new sparklineWidget(settings));
+        }
+    });
+
+}());
